cb77f90dd93a36bc75cf06e3a436c3589a000fcd
denizevliyaogluu <denizevliyaoglu01@gmail.com>
2024-06-05 20:37:21 +0300

    offer commit

--- resources/views/offers/index.blade.php
+++ resources/views/offers/index.blade.php
@@ -23,6 +23,7 @@
                     <tr>
                         <th>#</th>
                         <th>Product</th>
+                        <th>User</th>
                         <th>Price</th>
                         <th>Date</th>
                     </tr>
@@ -32,6 +33,15 @@
                         <tr>
                             <td>{{ $loop->iteration }}</td>
                             <td><a href="{{ route('products.show', $offer->product->uniqid) }}">{{ $offer->product->name }}</a></td>
+                            <td>
+                                <a href="#" class="user-info-link" data-toggle="modal" data-target="#userModal"
+                                   data-name="{{ $offer->user->name }}"
+                                   data-surname="{{ $offer->user->surname }}"
+                                   data-email="{{ $offer->user->email }}"
+                                   data-phone="{{ $offer->user->phone }}">
+                                   User Details
+                                </a>
+                            </td>
                             <td>{{ $offer->price }}</td>
                             <td>{{ $offer->created_at }}</td>
                         </tr>
@@ -41,7 +51,27 @@
         @endif
     </div>
 
-
+    <!-- User Info Modal -->
+    <div class="modal fade" id="userModal" tabindex="-1" role="dialog" aria-labelledby="userModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="userModalLabel">User Information</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p><strong>Name:</strong> <span id="modalUserName"></span></p>
+                    <p><strong>Surname:</strong> <span id="modalUserSurname"></span></p>
+                    <p><strong>Email:</strong> <span id="modalUserEmail"></span></p>
+                    <p><strong>Phone:</strong> <span id="modalUserPhone"></span></p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     @include('layouts.footer')
@@ -49,7 +79,21 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+    <script>
+        $(document).ready(function(){
+            $('.user-info-link').on('click', function(){
+                var name = $(this).data('name');
+                var surname = $(this).data('surname');
+                var email = $(this).data('email');
+                var phone = $(this).data('phone');
 
+                $('#modalUserName').text(name);
+                $('#modalUserSurname').text(surname);
+                $('#modalUserEmail').text(email);
+                $('#modalUserPhone').text(phone);
+            });
+        });
+    </script>
 
 </body>
 
